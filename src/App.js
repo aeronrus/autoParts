c6a85dbea87c09c289875ecab42c4bb0ade57caf
Ruslan Rogalev <rre@m-ppo.ru>
2023-08-11 16:31:21 +0300

    added all components and all buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import { useEffect } from 'react';
-
-const tg = window.Telegram.WebApp; //начала надо установить!!!
+import { tg, useTelegram } from './hooks/useTelegram';
+import Form from './components copy/Form/Form';
+import Header from './components copy/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import ProductList from './components copy/ProductList/ProductList';
 
 function App() {
+  const { tg, onToggleButton, handleClose } = useTelegram();
   useEffect(() => {
     tg.ready(); //метод сообщает, что приложение проинициализировалось и его можно отрисовывать
   }, []);
 
-  const handleClose = () => {
-    tg.close();
-  };
   return (
     <div className="App">
-      Реакт-апп
+      <Header />
+      <Routes>
+        <Route path="/form" element={<Form />}></Route>
+        <Route path="/products" element={<ProductList />}></Route>
+      </Routes>
+      <button onClick={onToggleButton}>toggle</button>//при нажатии на кнопку будет появлятьс или
+      исчезать mainbutton-основная кнопка взаимодействия с ботом
       <button onClick={handleClose}>Закрыть</button>
     </div>
   );
