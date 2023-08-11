95051da517922f1e9a1772b07eb74557ff410d58
Ruslan Rogalev <rre@m-ppo.ru>
2023-08-11 20:51:17 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { tg, useTelegram } from './hooks/useTelegram';
 import Form from './components copy/Form/Form';
 import Header from './components copy/Header/Header';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import ProductList from './components copy/ProductList/ProductList';
 
 function App() {
@@ -15,9 +15,11 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Link to="/form">Form</Link>
+      <h1>Change</h1>
       <Routes>
-        <Route path="form" element={<Form />}></Route>
-        <Route path="/" element={<ProductList />}></Route>
+        <Route index element={<ProductList />} />
+        <Route path={'form'} element={<Form />} />
       </Routes>
       <button onClick={onToggleButton}>toggle</button>
       <button onClick={handleClose}>Закрыть</button>
